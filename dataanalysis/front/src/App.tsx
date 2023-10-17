0b50304f6f1f969cff1c63cca218f7b84f999acb
Echo <sshe0364@uni.sydney.edu.au>
2023-10-17 20:02:31 +1100

    protect route

--- dataanalysis/front/src/App.tsx
+++ dataanalysis/front/src/App.tsx
@@ -4,15 +4,20 @@ import Login from './components/Login';
 import Register from './components/Register';  
 import DataRetrieval from './components/DataRetrieval';
 
+const ProtectedRoute: React.FC<{ path: string, element: React.ReactNode }> = ({ path, element }) => {
+    const isLoggedIn = Boolean(localStorage.getItem('token'));
+
+    return isLoggedIn ? <Route path={path} element={element} /> : <Navigate to="/login" />;
+}
+
 const App: React.FC = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
-                <Route path="/retrieval" element={<DataRetrieval />} />
+                <ProtectedRoute path="/retrieval" element={<DataRetrieval />} />
                 <Route path="*" element={<Navigate to="/login" />} /> {/* Default route redirects to login */}
-                {/* Add other routes as needed */}
             </Routes>
         </Router>
     );
