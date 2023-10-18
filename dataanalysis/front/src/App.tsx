7d8124aa9c4c0e430e3be78acd2aad5c105c4538
Echo <sshe0364@uni.sydney.edu.au>
2023-10-18 13:05:29 +1100

    chat page test

--- dataanalysis/front/src/App.tsx
+++ dataanalysis/front/src/App.tsx
@@ -4,37 +4,54 @@ import { AuthProvider, useAuth } from './AuthContext';
 import Login from './components/Login';
 import Register from './components/Register';
 import DataRetrieval from './components/DataRetrieval';
+import Chat from './components/Chat';
 import NotFound from './components/NotFound';
 
-const AppRoutes: React.FC = () => {
-    const { isLoggedIn } = useAuth(); // 移动到这里
 
-    if (isLoggedIn) {
-        return (
-            <Routes>
-                <Route path="/retrieval" element={<DataRetrieval />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-        );
-    } else {
-        return (
-            <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="*" element={<Navigate to="/login" />} />
-            </Routes>
-        );
-    }
-};
 
 const App: React.FC = () => {
-    return (
-        <AuthProvider>
-            <Router>
-                <AppRoutes />
-            </Router>
-        </AuthProvider>
-    );
-};
+    return(
+        //没有路由保护
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/register" element={<Register />} />
+                <Route path="/retrieval" element={<DataRetrieval />} />
+                <Route path='/chat' element={<Chat/>}/>
+            </Routes>
+        </Router>
+    )
+}
+
+// 路由保护，先别启用
+// const AppRoutes: React.FC = () => {
+//     const { isLoggedIn } = useAuth(); 
+
+//     if (isLoggedIn) {
+//         return (
+//             <Routes>
+//                 <Route path="/retrieval" element={<DataRetrieval />} />
+//                 <Route path="*" element={<NotFound />} />
+//             </Routes>
+//         );
+//     } else {
+//         return (
+//             <Routes>
+//                 <Route path="/login" element={<Login />} />
+//                 <Route path="/register" element={<Register />} />
+//                 <Route path="*" element={<Navigate to="/login" />} />
+//             </Routes>
+//         );
+//     }
+// };
+// const App: React.FC = () => {
+//     return (
+//         <AuthProvider>
+//             <Router>
+//                 <AppRoutes />
+//             </Router>
+//         </AuthProvider>
+//     );
+// };
 
 export default App;
