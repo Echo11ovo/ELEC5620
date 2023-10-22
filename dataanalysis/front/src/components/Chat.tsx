f1a6fe9b07c433cd7536d8926fb0728d0a8da293
Echo <sshe0364@uni.sydney.edu.au>
2023-10-22 22:21:14 +1100

    fix proxy bug

--- dataanalysis/front/src/components/Chat.tsx
+++ dataanalysis/front/src/components/Chat.tsx
@@ -8,8 +8,6 @@ type Message = {
     content: string;
 };
 
-
-
 const BACKEND_URL = 'http://localhost:5000';
 
 const loadingIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
@@ -19,9 +17,9 @@ function Chat() {
     const [userInput, setUserInput] = useState<string>('');
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const { Option } = Select;
-    const [userType, setUserType] = useState<string | null>(localStorage.getItem('userType')); 
-    //test
-    console.log("User Type:", userType); 
+    const [selectedPrompt, setSelectedPrompt] = useState<string>('');
+    const [userType, setUserType] = useState<string | null>(localStorage.getItem('userType'));
+    console.log("User Type:", userType);
     const handleFileUpload = async (info: any) => {
         if (info.file.status === 'done') {
             setMessages(prevMessages => [...prevMessages, { type: 'file-response', content: info.file.response.message }]);
@@ -57,7 +55,7 @@ function Chat() {
             const response = await fetch(`${BACKEND_URL}/api/chat`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ message: userInput })
+                body: JSON.stringify({ message: userInput , prompt: selectedPrompt })
             });
             const data = await response.json();
             setMessages(prevMessages => [...prevMessages, { type: 'chat-response', content: data.message }]);
@@ -74,15 +72,19 @@ function Chat() {
             <Col xs={24} sm={22} md={18} lg={14} xl={12}>
                 <div className="header">
                     <h2>intelligent data analysis</h2>
-                    <Select placeholder="Select a prompt" className="select">
-                    {availablePrompts.map(prompt => (
-                        <Option value={prompt.replace(/\s+/g, '-').toLowerCase()} key={prompt}>
-                            {prompt}
-                        </Option>
-                    ))}
-                </Select>
+                    <Select
+                        placeholder="Select a prompt"
+                        className="select"
+                        onChange={(value) => setSelectedPrompt(value as string)}
+                    >
+                        {availablePrompts.map(prompt => (
+                            <Option value={prompt.replace(/\s+/g, '-').toLowerCase()} key={prompt}>
+                                {prompt}
+                            </Option>
+                        ))}
+                    </Select>
                 </div>
-                
+
                 <div className="chatBox" role="log" aria-live="polite">
                     {messages.map((message, index) => (
                         <div key={index} className={`message ${message.type}`}>
@@ -90,24 +92,24 @@ function Chat() {
                         </div>
                     ))}
                 </div>
-        
+
                 <div className="inputBox">
-                    <Input 
-                        type="text" 
-                        value={userInput} 
-                        onChange={(e) => setUserInput(e.target.value)} 
-                        placeholder="Type your message..." 
+                    <Input
+                        type="text"
+                        value={userInput}
+                        onChange={(e) => setUserInput(e.target.value)}
+                        placeholder="Type your message..."
                     />
                     <Button type="primary" onClick={handleSendMessage} disabled={isLoading}>
                         {isLoading ? <Spin indicator={loadingIcon} /> : "Send"}
                     </Button>
                 </div>
-        
+
                 <div className="uploadBox">
-                    <Upload 
-                        name="file" 
-                        action={`${BACKEND_URL}/api/upload`}  
-                        showUploadList={false} 
+                    <Upload
+                        name="file"
+                        action={`${BACKEND_URL}/api/upload`}
+                        showUploadList={false}
                         onChange={handleFileUpload}
                         aria-label="Upload a file for analysis">
                         <Button icon={<UploadOutlined />}>Upload a file for analysis</Button>
